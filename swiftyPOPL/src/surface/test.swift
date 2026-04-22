2656a0e57aa46219311fbc106f8cda708ad09f9a
uniqueFranky <y_r_b@qq.com>
2026-04-22 13:42:59 +0800

    add basic syntax for surface

--- swiftyPOPL/src/surface/test.swift
+++ swiftyPOPL/src/surface/test.swift
@@ -1,3 +1,19 @@
-func testFunc(x: Int) -> Int {
-    3
+let f: (Int, Int) -> Bool = func (_: Int, val: Int) -> Bool {
+    false
 }
+
+struct S {
+    let x: Int
+    let y: Bool
+    func foo(_ x: Int) -> Bool {
+        let z: Bool = false
+        self.y
+    }
+}
+
+protocol P {
+    let x: Int
+    func foo(_ x: Int) -> Bool
+}
+
+
